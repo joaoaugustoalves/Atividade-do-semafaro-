e3e7ce3d2d5f1ef4747b2a6b0e022be096a3546c
Joao Augusto <frajolacommel@gmail.com>
2020-11-16 02:50:45 -0300

    semafaro feito

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Semáforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a cor do semáforo (verde,vermelho,amarelo):");
+            string cordosem = (Console.ReadLine());
+
+            if(cordosem == "vermelho") {
+                Console.WriteLine("Pare!");
+                    }
+            else if(cordosem == "amarelo") {
+                Console.WriteLine("Reduza e Pare!");
+                    }
+            else {
+                Console.WriteLine("Siga !");
+                    }   
+            
+        }
+    }
+}
